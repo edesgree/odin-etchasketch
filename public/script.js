2b852c2aa6ca205782f6546e5c7871f616975240
Edouard Desgrée <edouard.desgree@gmail.com>
2022-10-11 05:10:11 +0200

    js cleanup

--- public/script.js
+++ public/script.js
@@ -1,13 +1,12 @@
 // Etch a sketch game
 const game = () => {
 
-    console.log("hello");
     // init variables/
     const gridElements = 16;
     const modes = ['colorMode', 'rainbowMode', 'eraserMode'];
     let currentColor = '#333';
     let currentMode = modes[0];
-    // draw the grid
+
     // UI elememts
     const gridContainer = document.getElementById('grid');
     const pixelHTML = document.createElement('div');
@@ -18,12 +17,12 @@ const game = () => {
     const btnRainbow = document.getElementById('rainbowMode');
     const btnClear = document.getElementById('clear');
 
-    
-    
 
+
+    // grid creation function
     const createGrid = (gridSize) => {
         //reset grid
-        gridContainer.innerHTML="";
+        gridContainer.innerHTML = "";
         // add grid css to the grid element
         gridContainer.style.gridTemplateColumns = "repeat(" + gridSize + ",1fr)";
         pixelHTML.classList.add('pixel');
@@ -32,42 +31,42 @@ const game = () => {
             // create a clone each time
             gridContainer.appendChild(pixelHTML.cloneNode(true));
         }
-        
     }
 
+    //draw function
     const draw = () => {
 
         // change background color of target via event
         const changePixelColor = (e) => {
             e.preventDefault();
+            // check if target is a pixel (.pixel)
             if (e.target.classList.contains('pixel')) {
+                // rainbow mode: change color to random RGB value
                 if (currentMode === 'rainbowMode') {
                     e.target.style.backgroundColor = 'rgb(' + getRandomRGB() + ')';
-                } else if (currentMode === 'eraserMode') {
+                }
+                // eraser mode: change color to empty
+                else if (currentMode === 'eraserMode') {
                     e.target.style.backgroundColor = '';
-                } else {
+                }
+                // normal color mode: get the current color
+                else {
                     e.target.style.backgroundColor = currentColor;
                 }
-
             }
-
-
         }
-        // execute on click
+        // When mousedown on the grid...
         gridContainer.addEventListener('mousedown', function (e) {
             e.preventDefault();
             // call a first time to change colour on first mousedown
             changePixelColor(e);
             // then continue calling the colorchange on mouse over
             gridContainer.addEventListener('mouseover', changePixelColor);
-            console.log(currentMode);
         });
         // remove event on mouse up
-        gridContainer.onmouseup = function (e) {
+        gridContainer.onmouseup = function () {
             gridContainer.removeEventListener('mouseover', changePixelColor);
         }
-
-
     }
 
     // update current color
@@ -76,54 +75,54 @@ const game = () => {
     }
     // create grid
     createGrid(gridElements);
-    
-    
+
     // update color choice via color picker input
     inputColor.addEventListener('change', (e) => {
         updateColor(e.target.value);
         currentMode = modes[0];
-        console.log(currentMode);
     });
 
     //eraser mode
     btnEraser.addEventListener('click', () => {
         currentMode = modes[2];
-        console.log(currentMode);
     });
 
     //rainbow mode
     btnRainbow.addEventListener('click', () => {
         currentMode = modes[1];
-        console.log(currentMode);
     });
 
     //clear mode
     btnClear.addEventListener('click', () => {
+        // get all pixels in an array
         let pixels = Array.from(document.querySelectorAll('#grid .pixel'));
+        // assign empty to each pixel bg color
         pixels.forEach(pixel => pixel.style.backgroundColor = "");
-        console.log(pixels);
     });
 
-    //grid size input
+    //change of grid size when user use the range input
     inputGridSize.addEventListener('input', (e) => {
-        
+        // create new grid with new size
         createGrid(e.target.value);
+        // let the user know the grid size when input is changed
         inputGridSizeValue.innerHTML = e.target.value;
     });
+
     // getting random number between 2 values (useful for random RGB color)
     function getRandomIntInclusive(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
     }
+
     // getting a random RGB value
     function getRandomRGB() {
         let arrayRGB = [getRandomIntInclusive(0, 255), getRandomIntInclusive(0, 255), getRandomIntInclusive(0, 255)];
         // return RGB values (255,255,255)
         return arrayRGB.join();
     }
+    // call the draw function
     draw();
-    console.log(getRandomRGB());
 }
 
 //play game
