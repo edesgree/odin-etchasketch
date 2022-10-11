386101a32482f2d82c7ffdd69d9d2d74adff6ce7
Edouard Desgrée <edouard.desgree@gmail.com>
2022-10-11 02:05:25 +0200

    add rainbow mode rgb

--- public/script.js
+++ public/script.js
@@ -23,13 +23,26 @@ const game = () => {
     }
 
     const draw = () => {
-        
+
         // change background color of target via event
         const changePixelColor = (e) => {
             e.preventDefault();
             if (e.target.classList.contains('pixel')) {
-                e.target.style.backgroundColor = currentColor;
+                if(currentMode = 'rainbowMode'){
+                    e.target.style.backgroundColor = 'rgb('+getRandomRGB()+')';
+                }else if(currentMode='eraserMode'){
+                    e.target.style.backgroundColor = '';
+                }else{
+                    e.target.style.backgroundColor = currentColor;
+                }
+                
             }
+            // pixels.forEach(pixel => pixel.addEventListener('mouseover ', () => {
+            //     console.log('click');
+            //     console.log(this);
+            //     pixel.style.backgroundColor = "blue";
+            // }));
+
         }
         // execute on click
         gridContainer.addEventListener('mousedown', function (e) {
@@ -45,21 +58,33 @@ const game = () => {
             gridContainer.removeEventListener('mouseover', changePixelColor);
         }
 
-      
+
     }
-    
+
     // update current color
-    function updateColor(color){
+    function updateColor(color) {
         currentColor = color;
-    
     }
-    
+
     createGrid(gridElements);
     // update color choice via color picker input
     inputColor.addEventListener('change', function (e) {
         updateColor(e.target.value);
     });
+    // getting random number between 2 values (useful for random RGB color)
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+    }
+    // getting a random RGB value
+    function getRandomRGB() {
+        let arrayRGB = [getRandomIntInclusive(0, 255), getRandomIntInclusive(0, 255), getRandomIntInclusive(0, 255)];
+        // return RGB values (255,255,255)
+        return arrayRGB.join();
+    }
     draw();
+    console.log(getRandomRGB());
 }
 
 //play game
